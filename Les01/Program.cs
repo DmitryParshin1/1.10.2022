a0fe5d173f821c3ba157ea72234fbc2d70e3a06b
DmitryParshin <dima.rs@mail.ru>
2022-10-02 11:19:40 +0300

    сделали блок схему, диаграмму ии код задаче 2

---
+++ Les01/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Start");
+
+int a = -9;
+int b = -3;
+
+int max = 0;
+int min = 0;
+
+if (a > b)
+{
+    max = a;
+    min = b;
+}
+else
+{
+    max = b;
+    min = a;
+}
+
+Console.WriteLine("Максимальное число - " + max);
+Console.WriteLine("Минимальное число - " + min);
+
+
+Console.WriteLine("End");
