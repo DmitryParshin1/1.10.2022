a9f0ef0d2d7ab35c648df7ba281cae637fff07c6
DmitryParshin <dima.rs@mail.ru>
2022-10-02 13:04:55 +0300

    добавили блок схему и код на 3 задачу

---
+++ Les01/Les03/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Start");
+
+int a = 4;
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное = " + a);
+}
+else
+{
+    Console.WriteLine("Число нечетное" + a);
+}
+Console.WriteLine("End");
