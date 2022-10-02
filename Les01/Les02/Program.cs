b1aaac1bfb2bffb1f10806e0d1775bee95c28b93
DmitryParshin <dima.rs@mail.ru>
2022-10-02 12:48:17 +0300

    решили вторую задачу, сделали код и блок схему

---
+++ Les01/Les02/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Start");
+
+int a = 2;
+int b = 3;
+int c = 7;
+int max = a; 
+
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.WriteLine("Максимальное значение - " + max);
+
+Console.WriteLine("END");
