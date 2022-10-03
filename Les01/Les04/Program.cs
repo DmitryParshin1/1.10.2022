04363230f76b9e801c46da027ed18fee5f7d3ee5
DmitryParshin <dima.rs@mail.ru>
2022-10-03 15:56:25 +0300

    добавил код в 4 задаче с методом, так же на семинарах сделали 5 задачу

---
+++ Les01/Les04/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+int Method(string text)
+{
+    Console.Write(text + " : ");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+void Method2(int a)
+{
+    int count = 2;
+    while (count < a)
+    {
+        Console.WriteLine($"{count}");
+        count += 2;
+    }
+}
+int N = Method("введите число");
+Method2(N);
---
+++ Les01/Les05/Program.cs
@@ -0,0 +1,25 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int Method()
+{
+int result = new Random().Next(10,100);
+Console.WriteLine($"наше число: {result}");
+return result;
+}
+void Method2(int number)
+{
+    int digit1 = number%10;
+    int digit2 = number/10;
+
+    if (digit1 > digit2)
+    {
+        Console.WriteLine($"наибольшая цифра - {digit1}");
+    }
+    else 
+    {
+        Console.WriteLine($"наибольшая цифра - {digit2}");
+    }
+}
+
+int wel = Method();
+Method2(wel);
