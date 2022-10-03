14000034f0eb327f9ddcdfc67a56aeba1ce90b81
DmitryParshin <dima.rs@mail.ru>
2022-10-04 00:33:18 +0300

    папки array это с лекции коды, перепечатывал и вникал

---
+++ Les01/array/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = { 22, 54, 56, 76, 35, 89, 76, 43, 50 };
+
+int n = array.Length;
+int find = 76;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
