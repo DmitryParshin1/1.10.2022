064218e334df86342a48eaee2abc7cc7c59265d1
DmitryParshin <dima.rs@mail.ru>
2022-10-04 18:52:22 +0300

    решение задачи 10

---
+++ Les01/Les07/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 10: Напишите метод, который принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+
+int Method()
+{
+int result = 456;
+Console.WriteLine($"наше число: {result}");
+return result;
+}
+void Method2(int number)
+{
+    int result = (number / 10) % 10;
+    Console.WriteLine($"Вторая цифра числа - {result}!");
+}
+
+int wel = Method();
+Method2(wel);
