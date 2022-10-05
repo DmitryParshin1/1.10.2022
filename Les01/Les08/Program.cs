bba767dd36fac83cf26ca27e0e35b19d40435939
DmitryParshin <dima.rs@mail.ru>
2022-10-05 19:25:16 +0300

    решение домашнего задания - задача Задача 13:

--- Les01/Les08/Program.cs
+++ Les01/Les08/Program.cs
@@ -1,1 +1,37 @@
-﻿
+﻿//Задача 13: Напишите метод, который выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+
+int Method(string text)
+{
+    Console.Write(text + " : ");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+void Method2(int number)
+{
+    if (number % 100 == number)
+    {
+        Console.WriteLine($"В нашем числе {number} нет третьей цифры");
+    }
+    else 
+    {
+        int digit = number % 10;
+        while (true)
+        {
+            if (number % 1000 == number)
+            {
+                Console.WriteLine($"Третья цифра - {digit}");
+                break;
+            }
+            else
+            {
+               number /= 10;
+            }
+        }
+    }
+}
+    int N = Method("введите число");
+    Method2(N);
