79077473f9894783f030681d71c571634e3d2f70
DmitryParshin <dima.rs@mail.ru>
2022-10-07 17:40:09 +0300

    добавил решение задачи 16

---
+++ Les01/Les09/Program.cs
@@ -0,0 +1,30 @@
+﻿//Напишите метод, который принимает на вход цифру,
+//обозначающую день недели, и проверяет, является ли этот день выходным.
+//6 -> да
+//7 -> да
+//1 -> нет
+
+
+int Weekend(string text)
+    {
+      int number = 0;
+      Console.WriteLine("Введите день недел от 1 до 7: ");
+      int value = int.Parse(Console.ReadLine());
+      return value;
+    }
+
+void WeekDay(int number)
+{
+    if ((number % 6 == 0) || (number % 7 == 0))
+    {
+        Console.WriteLine("Выходной!");
+    }
+    else
+    {
+        Console.WriteLine("Опять работать");
+    }
+}
+
+Console.Clear();
+int day = Weekend("Введите день недел от 1 до 7: ");
+WeekDay(day);
--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// a + b = c
+
+int Method(string text)
+{
+    Console.WriteLine("введите числа" + text);
+    int value = int.Parse(Console.ReadLine());
+    return value
+}
+int Method2(int c)
+{
+    a = int.Parse(Console.ReadLine());
+    //Console.WriteLine("введите число" + a);
+    b = int.Parse(Console.ReadLine());
+    //Console.WriteLine("введите число" + b);
+    int c = a + b;
+}
+
+int N = Method("введите числа");
+Method2(N);
