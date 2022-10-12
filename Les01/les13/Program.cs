45250a115d58e802e5f70374350edd47ed3a1f4a
DmitryParshin <dima.rs@mail.ru>
2022-10-12 23:01:57 +0300

    добавлена задача с палиндромом

--- Les01/les13/Program.cs
+++ Les01/les13/Program.cs
@@ -4,3 +4,33 @@
 //23432 -> да
 
 
+int red = getNumber("Введите числа: ");
+if(Palindrom(red))
+{
+    Console.WriteLine($"Данное число {red} является полидромом");
+}
+else
+{
+    Console.WriteLine($"Данное число {red} не является полидромом");
+}
+
+int getNumber(string text)
+{
+ Console.Write(text + " : ");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+bool Palindrom(int number)
+{
+    int TempNumber = number / 10;
+    int Reverse = number % 10;
+    do
+    {
+        Reverse = Reverse * 10 + TempNumber % 10;
+        TempNumber = TempNumber / 10;
+    }
+    while(TempNumber != TempNumber % 10);
+    Reverse = Reverse * 10 + TempNumber % 10;
+    return Reverse == number;
+}
