bef8536b253be381ca0e518918d4d6cf04510057
DmitryParshin <dima.rs@mail.ru>
2022-10-14 23:40:28 +0300

    добавлена задача со степенью 25-номер

---
+++ Les01/Les16/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 25: опишите метод, который принимает на вход два числа (A и B) и возводит число A в целую степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+//без math.pow
+
+int get(string text)
+{
+    Console.WriteLine(text + " : ");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int numbers(int firstNumber, int secondNumber)
+{
+int result = 1;
+for (int i = 0; i < secondNumber; i++)
+{
+    result = result * firstNumber;
+}
+return result;
+}
+
+void arr(int firstNumber, int secondNumber, int result)
+{
+    Console.WriteLine($"{firstNumber}, {secondNumber} -> {result}");
+}
+
+int firstNumber = get("Введите число");
+int secondNumber = get("Введите степень");
+arr(firstNumber, secondNumber, numbers(firstNumber, secondNumber));
+
---
+++ Les01/les14/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+
+int getValue(string text)
+{
+    Console.WriteLine(text + " : ");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int getRes(int number)
+{
+    int firstNumber = number % 10;
+    int secondNumber = number / 10;
+    int sum = firstNumber + secondNumber;
+    Console.WriteLine(sum);
+    return sum;
+   }
+
+Console.Clear();
+int qwerty = getValue("Введиите число");
+getRes(qwerty);
+
