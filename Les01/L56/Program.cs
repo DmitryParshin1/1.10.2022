d2b917ba3a941b945b44b68e17a8e00539249cc8
DmitryParshin <dima.rs@mail.ru>
2022-10-26 20:29:14 +0300

    пытаюсь решить задачу 57

---
+++ Les01/L56/Program.cs
@@ -0,0 +1,37 @@
+﻿// Написать программу, которая обменивает элементы первой строки и последней строки
+
+int[,] Matrix(int[,] arr)
+{
+    int A = arr.GetLength(0);
+    int B = arr.GetLength(1);
+    int[,] res = new int [A, B];
+    for (int j = 0; j < A; j++)
+    {
+        for (int i = 0; i < B; i++)
+        {
+            res[A - 1 - i, j] = arr[i, j];
+        }
+    }
+    return res;
+}
+
+void Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            //arr[i, j] = new Random().Next(1,10);
+            Console.Write($" {arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] ar1 = {{1, 2, 3},
+              {4, 5, 6},
+              {7, 8, 9}};
+var ar2 = Matrix(ar1);
+Print(ar2);
+
---
+++ Les01/L57/Program.cs
@@ -0,0 +1,44 @@
+﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+
+int[,] Array(int[,] number)
+{
+    for (int i = 0; i < number.GetLength(0); i++)
+    {
+        for (int j = 0; j < number.GetLength(1); j++)
+        {
+            number[i, j] = new Random().Next(1,10);
+            Console.Write($" {number[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    return number;
+}
+Console.WriteLine();
+
+
+
+int[,] NextArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1) - 1; k++)
+            {
+                if(arr[i,k] < arr[i, k + 1])
+                {
+                    int sum = arr[i, k + 1];
+                    arr[i, k + 1] = arr[i,k];
+                    arr[i,k] = sum;
+                    Console.Write($" {sum} ");
+                }
+            }
+        }
+    }
+    return arr;
+}
+Console.WriteLine();
+
+int[,] day = new int [3, 3];
+day = Array(day);
+Console.WriteLine($" {NextArray(day)} ");
