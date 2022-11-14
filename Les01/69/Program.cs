c8ddb9580a7a89f2bba9c65708252a7bad9dae23
DmitryParshin <dima.rs@mail.ru>
2022-11-14 17:51:36 +0300

    добавил решение задачи

--- Les01/69/Program.cs
+++ Les01/69/Program.cs
@@ -1,6 +1,6 @@
-﻿// 69. Найти сумму элементов от M до N, N и M заданы
+﻿//69. Найти сумму элементов от M до N, N и M заданы
 
-/*Console.WriteLine(" Введите число А:  ");
+Console.WriteLine(" Введите число А:  ");
 int NumberA = int.Parse(Console.ReadLine());
 
 Console.WriteLine(" Введите число B:  ");
@@ -18,13 +18,13 @@ void NumberSum(int NumberA, int NumberB, int Sum)
 }
 
 NumberSum(NumberA, NumberB, 0);
-*/
 
-int SumRec(int n)
+
+/*int SumRec(int n)
 {
      if(n == 0) return 0;
     else return n + SumRec(n -1);
 }
 
-Console.WriteLine(SumRec(10));
+Console.WriteLine(SumRec(10));*/
 
---
+++ Les01/71/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 71: Напишите программу вычисления функции Аккермана с помощью
+рекурсии. Даны два неотрицательных числа m и n.
+m = 3, n = 2 -> A(m,n) = 29
+*/
+
+
+Console.WriteLine(" Введите число N ");
+int numberN = int.Parse(Console.ReadLine());
+
+Console.WriteLine(" Введите число M ");
+int numberM = int.Parse(Console.ReadLine());
+
+int Ackermann(int numberM, int numberN)
+{
+    if(numberM == 0) return numberN + 1;
+    else 
+    if ((numberM > 0) && (numberN == 0)) 
+    return Ackermann(numberM - 1, 1);
+
+    else 
+   //if ((numberM > 0) && (numberN > 0)) 
+    return Ackermann(numberM - 1, Ackermann(numberM, numberN - 1));
+}
+
+
+Console.WriteLine($"Аккерман для чисел m = {numberM}, n = {numberN}  --- A(m,n) = {Ackermann(numberM, numberN)}");
