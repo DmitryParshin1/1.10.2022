f92c196281caf650e441eb47f63194a6781d08f4
DmitryParshin <dima.rs@mail.ru>
2022-11-14 19:29:51 +0300

    добавил решение задачи

--- Les01/63/Program.cs
+++ Les01/63/Program.cs
@@ -5,3 +5,39 @@
 34(1,0,0) 41(1,1,0)
 27(0,0,1) 90(0,1,1)
 26(1,0,1) 55(1,1,1) */
+
+int[,,] Array(int[,,] arr, int numberA, int numberB)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                arr[i, j , k] = Random.Shared.Next(numberA, numberB + 1);
+            }
+        }
+    }
+    return arr;
+}
+
+void Print(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($" {array[i, j, k]} ({i}, {j}, {k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int[,,] matrix = new int[3, 3, 3];
+
+matrix = Array(matrix, 10 , 99);
+Print(matrix);
+Console.WriteLine();
